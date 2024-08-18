b4d211e149b9b90d1d120968ebcefb04b1490879
Joey <joeyhubbard2020@outlook.com>
2024-08-18 03:05:41 -0400

    Task 1 and 2 complete

--- final_project/index.js
+++ final_project/index.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 // Authentication Mechanism
+
+/* Commenting out authorization to test CRUD without Auth for Tasks 1-5
 app.use("/customer/auth/*", function auth(req, res, next) {
     // check to see if user is logged in and has a valid token
     if (req.session.authorization) {
@@ -27,10 +29,11 @@ app.use("/customer/auth/*", function auth(req, res, next) {
         return res.status(403).json({ message: "User not logged in" });
     }
 });
- 
+*/
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
