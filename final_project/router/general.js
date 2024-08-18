8219a210b1fba433de8fa43ab6183bce41976336
Joey <joeyhubbard2020@outlook.com>
2024-08-18 03:51:02 -0400

    Task 3 - get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  //setting author strings to lowercase to ignore capitals when searching for a match
+  const searchedAuthor = req.params.author.toLowerCase()
+  // creates an array of isbns 
+  const bookIsbns = Object.getOwnPropertyNames(books)
+
+  let booksByAuthor = []
+  bookIsbns.forEach(isbn => {
+    let author = books[isbn].author.toLowerCase()
+    if (author.match(searchedAuthor)) {
+        booksByAuthor.push(books[isbn])
+    }
+  })
+
+  res.status(200).send(JSON.stringify(booksByAuthor, null, 4))
 });
 
 // Get all books based on title
