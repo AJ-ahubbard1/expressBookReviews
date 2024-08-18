bfa9459d2afddaa71fdfb2c8873a7f1804644623
Joey <joeyhubbard2020@outlook.com>
2024-08-18 04:13:17 -0400

    Task 4 and 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,24 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  /*
+  const username = req.body.username
+  const password = req.body.password
+
+  // Check if both username and password are provided
+  if (username && password) {
+    // Check if the user does not already exist
+    if (!doesExist(username)) {
+      // Add the new user to the users array
+      users.push({ username: username, password: password })
+      return res.status(200).json({ message: "User successfully registered. Now you can login" })
+    } else {
+      return res.status(404).json({ message: "User already exists!" })
+    }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({ message: "Unable to register user." })
+  */
 });
 
 
@@ -47,14 +63,30 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  //setting title strings to lowercase to ignore capitals when searching for a match
+  const searchedTitle = req.params.title.toLowerCase()
+  // creates an array of isbns 
+  const bookIsbns = Object.getOwnPropertyNames(books)
+
+  let booksByTitle = []
+  bookIsbns.forEach(isbn => {
+    let title = books[isbn].title.toLowerCase()
+    if (title.match(searchedTitle)) {
+        booksByTitle.push(books[isbn])
+    }
+  })
+  res.status(200).send(JSON.stringify(booksByTitle, null, 4))
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const book = books[isbn]
+  if (!book) {
+    return res.status(404).send({ error: "No book found" })
+  }
+  res.status(200).send(JSON.stringify(book.reviews, null, 4));
 });
 
 module.exports.general = public_users;
