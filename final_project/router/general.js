e05870a73b43b4cc852a95b6a37db7128fdb5734
Joey <joeyhubbard2020@outlook.com>
2024-08-18 15:49:50 -0400

    Complete Task 1-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,59 +26,79 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.status(200).send(JSON.stringify(books, null, 4))
+public_users.get('/', async (req, res) => {
+    try {
+        res.status(200).send(await JSON.stringify(books, null, 4))
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn
-  const book = books[isbn]
-  if (!book) {
-    return res.status(404).send({ error: "No book found" })
+public_users.get('/isbn/:isbn', async (req, res) => {
+  try {
+    const isbn = req.params.isbn
+    const book = books[isbn]
+    
+    if (!book) {
+      return res.status(404).send({ error: "No book found" })
+    }
+    res.status(200).send(await JSON.stringify(book, null, 4));
   }
-  res.status(200).send(JSON.stringify(book, null, 4));
+  catch (err) {
+    res.status(500).send(err)
+  }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async (req, res) => {
+  
+  try {
+    //setting author strings to lowercase to ignore capitals when searching for a match
+    const searchedAuthor = req.params.author.toLowerCase()
+    // creates an array of isbns 
+    const bookIsbns = Object.getOwnPropertyNames(books)
 
-  //setting author strings to lowercase to ignore capitals when searching for a match
-  const searchedAuthor = req.params.author.toLowerCase()
-  // creates an array of isbns 
-  const bookIsbns = Object.getOwnPropertyNames(books)
-
-  let booksByAuthor = []
-  bookIsbns.forEach(isbn => {
-    let author = books[isbn].author.toLowerCase()
-    if (author.match(searchedAuthor)) {
+    let booksByAuthor = []
+    bookIsbns.forEach(isbn => {
+      let author = books[isbn].author.toLowerCase()
+      if (author.match(searchedAuthor)) {
         booksByAuthor.push(books[isbn])
-    }
-  })
+      }
+    })
 
-  res.status(200).send(JSON.stringify(booksByAuthor, null, 4))
+    res.status(200).send(await JSON.stringify(booksByAuthor, null, 4))
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
+  try {
+    //setting title strings to lowercase to ignore capitals when searching for a match
+    const searchedTitle = req.params.title.toLowerCase()
+    // creates an array of isbns 
+    const bookIsbns = Object.getOwnPropertyNames(books)
 
-  //setting title strings to lowercase to ignore capitals when searching for a match
-  const searchedTitle = req.params.title.toLowerCase()
-  // creates an array of isbns 
-  const bookIsbns = Object.getOwnPropertyNames(books)
-
-  let booksByTitle = []
-  bookIsbns.forEach(isbn => {
-    let title = books[isbn].title.toLowerCase()
-    if (title.match(searchedTitle)) {
-        booksByTitle.push(books[isbn])
-    }
-  })
-  res.status(200).send(JSON.stringify(booksByTitle, null, 4))
+    let booksByTitle = []
+    bookIsbns.forEach(isbn => {
+        let title = books[isbn].title.toLowerCase()
+        if (title.match(searchedTitle)) {
+            booksByTitle.push(books[isbn])
+        }
+    })
+    res.status(200).send(await JSON.stringify(booksByTitle, null, 4))
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', (req, res) => {
   const isbn = req.params.isbn
   const book = books[isbn]
   if (!book) {
